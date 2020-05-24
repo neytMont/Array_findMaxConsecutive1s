e8da04c7af428677e5cb1d1582202540b198aad9
netFontero <59034202+netFontero@users.noreply.github.com>
2020-05-23 22:35:15 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,9 @@ public class Main
 		int[] arr = { 1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,0 };
 		System.out.println(findMaxConsecutiveOnes(arr));
 		
+		int[] arr1 = { 1,0,1,0,0,0,1,1,1,1,1,1,1,1,0,0,1};
+		System.out.println(findMaxConsecutiveOnes(arr1));
+		
 
 	}
 	
