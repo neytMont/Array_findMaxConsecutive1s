3b0f9d8c561be7a8fda3080ee6920b144c20d9eb
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2021-01-05 20:25:16 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,53 +5,40 @@
  * Explaination: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
  * @author Nathaniel Monte de Ramos
  */
-public class Main
-{
-
-	public static void main(String[] args) 
-	{
+public class Main{
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = { 1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,0 };
 		System.out.println(findMaxConsecutiveOnes(arr));
 		
 		int[] arr1 = { 1,0,1,0,0,0,1,1,1,1,1,1,1,1,0,0,1};
 		System.out.println(findMaxConsecutiveOnes(arr1));
-		
-
 	}
 	
-    public static int findMaxConsecutiveOnes(int[] nums) 
-    {
+	public static int findMaxConsecutiveOnes(int[] nums){
     	int count = 0;
     	int maxNum = 0;
     	
-    	for(int i = 0; i < nums.length; i++) 
-    	{
+    	for(int i = 0; i < nums.length; i++) {
     		//base case if nums[i] == 1 then count is 1
-    		if(nums[i] == 1) 
-    		{
+    		if(nums[i] == 1) {
     			count++;
     		}
-    		else
-            {
-                if(count > maxNum)
-                {
-                    maxNum = count;
-                }
-                if(maxNum >= nums.length / 2)
-                {
-                    return maxNum;
-                }
-                count = 0;
-            }
+    		else{
+                	if(count > maxNum){
+                    		maxNum = count;
+                	}
+                	if(maxNum >= nums.length / 2){
+                    		return maxNum;
+                	}
+                	count = 0;
+            	}
     	}
     	
-    	if(count < maxNum) 
-    	{
+    	if(count < maxNum) {
     		return maxNum;
     	}
-    	else 
-    	{
+    	else {
     		return count;
     	}
     }
