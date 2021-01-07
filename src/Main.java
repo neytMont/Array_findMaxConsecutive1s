fb11565c355f0bc484ecf7879abf6d97203008d9
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2021-01-06 21:04:54 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@
  * Input: [1,1,0,1,1,1]
  * Output: 3
  * Explaination: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
- * @author Nathaniel Monte de Ramos
  */
 public class Main{
 	public static void main(String[] args) {
